6142f317b342cf42ae4132148a396d0a89d407ce
Dmitry Ponyatov <dponyatov@gmail.com>
2016-08-19 12:29:34 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- cpp.cpp
+++ cpp.cpp
@@ -2,3 +2,10 @@
 #define YYERR "\n\n"<<yylineno<<":"<<msg<<"["<<yytext<<"]\n\n"
 void yyerror(string msg) { cout<<YYERR; cerr<<YYERR; exit(-1); }
 int main() { return yyparse(); }
+
+Sym::Sym(string T,string V) { tag=T; val=V; }
+Sym::Sym(string V):Sym("sym",V) {}
+
+string Sym::dump() { return "<"+tag+":"+val+">"; }
+
+Str::Str(string V):Sym("str",V){}
--- hpp.hpp
+++ hpp.hpp
@@ -8,14 +8,17 @@
 using namespace std;
 
 struct Sym {
-	string val;
-	Sym(string);
+	string tag,val;
+	Sym(string,string); Sym(string);
 	virtual string dump();
 };
+
+struct Str: Sym { Str(string); };
 
 extern int yylex();
 extern int yylineno;
 extern char* yytext;
+#define TOC(C,X) { yylval.o = new C(yytext); return X; }
 extern int yyparse();
 extern void yyerror(string);
 #include "ypp.tab.hpp"
